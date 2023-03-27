e139320033d256560265f56d5276903f2af9b74f
YG <66865438+CE-Walf@users.noreply.github.com>
2023-03-27 13:54:10 +0900

    [Bronze III] Title: 플러그, Time: 1252 ms, Memory: 312128 KB -BaekjoonHub

--- 백준/Bronze/2010. 플러그/플러그.java
+++ 백준/Bronze/2010. 플러그/플러그.java
@@ -5,12 +5,10 @@ public class Main{
         Scanner scanner = new Scanner(System.in);
         
         int N = scanner.nextInt();
-        int NArray[] = new int[N];
         int sum = 0;
         
         for(int i = 0; i < N ; i++){
-            NArray[i] = scanner.nextInt();
-            sum += NArray[i];
+            sum += scanner.nextInt();
             if(i != N-1)
                 sum--;
         }
