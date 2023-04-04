258b7cb7b488797d491b49eb9cd1e6a98c1b4797
YG <66865438+CE-Walf@users.noreply.github.com>
2023-04-04 13:24:25 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,33 +1,23 @@
 import java.util.Scanner;
 
 public class Main{
-	
+	static int getGCD(int a, int b) {
+		if(b == 0)
+			return a;
+		else
+			return getGCD(b, a%b);
+	}
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
 		int a = scanner.nextInt();
 		int b = scanner.nextInt();
-		int min = 0;
-		int result1 = 0;
-		int result2 = 0;
 		
-		if(a>b)
-			min = b;
-		else 
-			min = a;
+		int abGCD = getGCD(a,b);
+		int abLCM = abGCD * (a/abGCD) *(b/abGCD);
 		
-		//최대공약수
-		for(int i = min; i >0 ; i--) {
-			if((a % i == 0) && (b % i == 0)) {
-				result1 = i;
-				break;
-			}
-		}
-		System.out.println(result1); // 최대공약수 출력
-		
-		//최소공배수
-		result2 = result1 * a/result1 * b/result1; 
-		System.out.println(result2);
+		System.out.println(abGCD);
+		System.out.println(abLCM);
 		
 	}
 }
