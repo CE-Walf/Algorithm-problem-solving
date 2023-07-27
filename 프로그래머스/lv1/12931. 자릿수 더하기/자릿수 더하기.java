e5673f7eab3f142000064bf0a4284186203e9a4c
YG <66865438+CE-Walf@users.noreply.github.com>
2023-07-27 14:43:38 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,10 +4,12 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        String nString = Integer.toString(n);
-        for(int i = 0; i < nString.length(); i++)
-            answer += (int)(nString.charAt(i) - '0');
-
+        String N = Integer.toString(n);
+        
+        for(int i = 0; i < N.length(); i++){
+            answer += N.charAt(i) - '0';        
+        }
+        
         return answer;
     }
 }
