e9a18fa38c0133086831aeb519bc7cfbc86603eb
YG <66865438+CE-Walf@users.noreply.github.com>
2023-08-05 17:56:03 +0900

    [level 0] Title: 이진수 더하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.py
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.py
@@ -2,7 +2,6 @@ def solution(bin1, bin2):
     bin1 = int('0b' + bin1, 2)
     bin2 = int('0b' + bin2, 2)
     
-    answer = str(bin(bin1 + bin2))
-    answer = answer.replace('0b', '')
+    answer = str(bin(bin1 + bin2))[2:]
     
     return answer
