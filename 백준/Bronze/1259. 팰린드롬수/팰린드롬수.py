9abf696fea463f697bc6c84922f84ae12a618408
YG <66865438+CE-Walf@users.noreply.github.com>
2023-08-13 02:18:34 +0900

    [Bronze I] Title: 팰린드롬수, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,12 +1,10 @@
-import math
-
 while True:
   number = input()
   if number == '0':
     break
   
   answer = 'yes'
-  for i in range(0,math.floor(len(number)//2)):
+  for i in range(0,len(number)//2):
     if number[i] != number[-i-1]:
       answer = 'no'
   
