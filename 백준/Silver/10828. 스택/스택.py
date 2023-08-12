ec234d5c117bb973f8be6e3480a914a0ce4bbb7a
YG <66865438+CE-Walf@users.noreply.github.com>
2023-08-12 14:07:53 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -7,21 +7,12 @@ for i in range(N):
   command = sys.stdin.readline().split()
   
   if command[0] == "push":
-    stack.append(command[1])
+    stack.append(int(command[1]))
   elif command[0] == "pop":
-    if len(stack) > 0:
-      print(stack.pop())
-    else:
-      print(-1)
+    print(stack.pop()) if len(stack) > 0 else print(-1)
   elif command[0] == "top":
-    if len(stack) > 0:
-      print(stack[-1])
-    else:
-      print(-1)
+    print(stack[-1]) if len(stack) > 0 else print(-1)
   elif command[0] == "size":
     print(len(stack))
   elif command[0] == "empty":
-    if len(stack) > 0:
-      print(0)
-    else:
-      print(1)
+    print(0) if len(stack) > 0 else print(1)
