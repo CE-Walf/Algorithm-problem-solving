a6fe45cc9c1d73262c0f0f886c7c4441c19a72fc
YG <66865438+CE-Walf@users.noreply.github.com>
2023-08-25 22:06:02 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,5 +1,9 @@
 def solution(my_string, is_suffix):
     
-    answer = int(my_string.endswith(is_suffix))
+
+    if my_string[-len(is_suffix):] == is_suffix:
+        answer = 1
+    else:
+        answer = 0
         
     return answer
