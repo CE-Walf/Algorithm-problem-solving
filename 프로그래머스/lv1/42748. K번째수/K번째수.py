3fe1d112bc28436392a592bc08ccda95f1f326bc
YG <66865438+CE-Walf@users.noreply.github.com>
2023-09-25 18:34:43 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,11 +1,7 @@
 def solution(array, commands):
     answer = []
     
-    for command in commands:
-        i = command[0]
-        j = command[1]
-        k = command[2]
-        
+    for i, j, k in commands:
         temp_list = array[i-1:j]
         temp_list.sort()
         answer.append(temp_list[k-1])
