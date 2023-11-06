ad342c81324824c9373f4eb6ad27a22d943e830a
YG <66865438+CE-Walf@users.noreply.github.com>
2023-11-06 12:31:00 +0900

    [level 1] Title: 폰켓몬, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -2,16 +2,10 @@ def solution(nums):
     n = len(nums) # 폰켓몬의 갯수
     choose = len(nums) // 2 # 고를 수 있는 폰켓몬의 수
     
-    hashDict = dict()
-    
-    for num in nums:
-        if hash(num) in hashDict:
-            hashDict[hash(num)] += 1 
-        else: 
-            hashDict[hash(num)] = 1
+    len_nums= len(list(set(nums)))
             
-    if len(hashDict) <= choose:
-        answer = len(hashDict)
+    if len_nums <= choose:
+        answer = len_nums
     else:
         answer = choose
         
