d14949f4321256452fd0807e413584b5da12d662
YG <66865438+CE-Walf@users.noreply.github.com>
2023-12-27 01:33:04 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 14012 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // '\'를 표현하기 위해서는, 앞에 '\'를 붙여야한다.
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
