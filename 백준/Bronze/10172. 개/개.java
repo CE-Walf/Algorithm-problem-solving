0cc81da6b2fe30f4f08942d1c586ba8c41aed239
YG <66865438+CE-Walf@users.noreply.github.com>
2023-12-27 01:44:50 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // '\'를 표현하기 위해서는, 앞에 '\'를 붙여야한다.
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
