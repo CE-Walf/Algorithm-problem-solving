969f7dbc98f13f8370a160b9e04017cff035c535
YG <66865438+CE-Walf@users.noreply.github.com>
2023-12-30 23:18:19 +0900

    [Bronze II] Title: 공 바꾸기, Time: 248 ms, Memory: 18540 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,28 +1,29 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        
-        int N = scanner.nextInt(); 
-        int M = scanner.nextInt(); 
-        int temp;
-        
-        int basket[] = new int[N];
-        
-        for(int index = 1 ; index <= N ; index++){
-            basket[index-1] = index;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        // N번째 바구니에 N을 대입하며 초기화
+        int[] basket = new int[N+1];
+        for(int i = 1; i <= N; i++)
+            basket[i] = i;
+
+        // M번 공을 교환
+        for(int i = 0; i < M; i++){
+            int exchangeI = sc.nextInt();
+            int exchangeJ = sc.nextInt();
+            int temp = 0; // 바꾸어주기 위한 임시 변수
+
+            temp = basket[exchangeJ];
+            basket[exchangeJ] = basket[exchangeI];
+            basket[exchangeI] = temp;
         }
-        
-        for(int index1 = 0; index1 < M; index1++){
-            int i = scanner.nextInt();
-            int j = scanner.nextInt();
-            
-            temp = basket[i-1];
-            basket[i-1] = basket[j-1];
-            basket[j-1] = temp;
-        }
-        for(int index = 0; index < N ; index++)
-            System.out.print(basket[index] + " ");
+
+        //출력
+        for(int i = 1; i <= N; i++)
+            System.out.print(basket[i] + " ");
     }
 }
