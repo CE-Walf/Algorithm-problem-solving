85ab970f157d9a944857c7f7a9513ce264d877d3
YG <66865438+CE-Walf@users.noreply.github.com>
2024-01-25 17:31:12 +0900

    [Silver IV] Title: 카드2, Time: 272 ms, Memory: 48932 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,28 +1,31 @@
-import java.util.Scanner;
+// Queue를 사용하여 문제를 풀어보자.
+import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        int n = scanner.nextInt();
-        int nArray[] = new int[2*n];
-        
-        int front = 0;
-        int rear = n-1;
-        
-        for(int i = front; i <=rear; i++){
-            nArray[i] = i+1;
-        }
-        
-        for(int i = n; i > 1; i--){
-            front++;
-            nArray[rear+1] = nArray[front];
-            rear++;
-            front++;
-        }
-        
-        System.out.println(nArray[front]);
+        Queue<Integer> queue = new LinkedList<>(); // 큐 객체 생성
 
+        int N = scanner.nextInt();
+        int answer;
+
+        // Queue에 입력
+        for (int i = 1;i <= N; i++){
+            queue.add(i);
+        }
+
+        // 작업 수행 (하나 있을때까지)
+        while(true){
+            if(queue.size() == 1){
+                answer = queue.remove();
+                break;
+            }
+            queue.remove();
+            queue.add(queue.remove());
+        }
+
+        System.out.println(answer);
     }
 }
