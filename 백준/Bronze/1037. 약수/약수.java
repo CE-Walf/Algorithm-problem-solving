1a51505f3226356f693c9e3fa7a864d29e378f4b
YG <66865438+CE-Walf@users.noreply.github.com>
2024-01-28 17:18:37 +0900

    [Bronze I] Title: 약수, Time: 212 ms, Memory: 17828 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -2,20 +2,23 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main{
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		int divisorCount = scanner.nextInt();
-		int  divisorArray[] = new int[divisorCount];
-		
-		for(int i = 0; i < divisorCount; i++) 
-			divisorArray[i] = scanner.nextInt();
-		
-		Arrays.sort(divisorArray);
-		
-		int min = divisorArray[0];
-		int max = divisorArray[divisorCount-1];
-		
-		System.out.println(min * max);
-	}
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        // 배열 안에 입력 받는다. 1과 A를 제외한 약수를 받는다.
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(arr); // 해당 배열을 정렬해준다.
+
+        // 최솟값 * 최댓값이 A이므로, 그것들을 구해준다.
+        int min = arr[0];
+        int max = arr[arr.length-1];
+
+        System.out.println(min * max);
+    }
 }
