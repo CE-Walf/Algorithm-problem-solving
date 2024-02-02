282ecb6f4ac51ecb9f87ab3293963ea1e4b4a2bc
YG <66865438+CE-Walf@users.noreply.github.com>
2024-02-02 20:01:15 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer;
+        
+        if (common[1] - common[0] == common[2]-common[1]){
+            int d = common[1] - common[0];
+            answer = common[common.length-1] + d;
+        } else {
+            int r = common[1] / common[0];
+            answer = common[common.length-1] * r;
+        }
+        
+        
+        return answer;
+    }
+}
