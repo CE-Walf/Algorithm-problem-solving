667d685492d47ead9bf0ff0c6ecea75737adbbbc
WALF <ce.walf@gmail.com>
2024-03-21 01:45:41 +0900

    [Silver III] Title: 소수 구하기, Time: 912 ms, Memory: 34856 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,27 +1,33 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         int M = scanner.nextInt();
         int N = scanner.nextInt();
-        
-        int[] prime = new int[N+1];
-        
-        prime[0] = prime[1] = 0;
-        
-        for(int i = 2; i <= N; i++)
-            prime[i] = i;
 
-        for(int i = 2; i < N; i++){
-            if(prime[i] == 0)continue;
-            for(int j = 2*i; j <= N; j += i)
-                prime[j] = 0;
-        }
+        // 에라토스 테네스의 체를 사용하자.
+        // 소수의 배수를 지워주는 방식
+        // 2부터 false인것이 소수, true인것이 소수가 아니다.
+        boolean[] isPrime = new boolean[N + 1]; // N까지. 초기값 false
+
+        // 0과 1은 소수가 아니다. 그러므로 미리 true 선언 해준다.
+        isPrime[0] = true;
+        isPrime[1] = true;
         
-        for(int i = M; i <= N; i++)
-            if(prime[i] != 0)
+        for (int i = 2; i <= N; i++){
+            if (!isPrime[i]){
+                for (int j = 2*i ; j <= N; j += i){
+                    isPrime[j] = true;
+                }
+            }
+        }
+
+        for (int i = M; i <= N; i++){
+            if(!isPrime[i]){
                 System.out.println(i);
+            }
+        }
     }
 }
