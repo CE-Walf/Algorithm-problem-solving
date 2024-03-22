9de5a2b18f006fa0931f5b037f9652f9d9a4abfe
WALF <ce.walf@gmail.com>
2024-03-22 17:48:07 +0900

    [Silver III] Title: 소수 구하기, Time: 888 ms, Memory: 34004 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,33 +1,39 @@
 import java.util.Scanner;
 
 public class Main{
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // M이상, N이하의 소수를 모두 출력
         int M = scanner.nextInt();
         int N = scanner.nextInt();
 
-        // 에라토스 테네스의 체를 사용하자.
-        // 소수의 배수를 지워주는 방식
-        // 2부터 false인것이 소수, true인것이 소수가 아니다.
-        boolean[] isPrime = new boolean[N + 1]; // N까지. 초기값 false
+        // 에라토스테네스의 체를 사용하자.
+        // N까지의 소수를 구할 isNotPrime 배열을 생성하자.
+        // N까지이므로, N+1의 크기로 배열을 생성해준다.
+        // 여기서 소수는 false이며, 소수가 아닌 값은 true이다.
+        boolean[] isNotPrime = new boolean[N+1];
 
-        // 0과 1은 소수가 아니다. 그러므로 미리 true 선언 해준다.
-        isPrime[0] = true;
-        isPrime[1] = true;
-        
+        // 우선 0과 1은 소수가 아니다. true로 바꿔준다.
+        isNotPrime[0] = true;
+        isNotPrime[1] = true;
+
+        // 에라토스테네스의 체를 구현해보자.
         for (int i = 2; i <= N; i++){
-            if (!isPrime[i]){
-                for (int j = 2*i ; j <= N; j += i){
-                    isPrime[j] = true;
+            if (!isNotPrime[i]){ // 기본값인 false일 경우, 그의 배수는 다 소수가 아니다.
+                for (int j = 2 * i; j <= N; j += i){ // i를 제외하고, 그의 배수들을 다 true로 바꿔준다.
+                    isNotPrime[j] = true;
                 }
             }
         }
 
+        // 이제, boolean 배열을 순회하면서, 소수인것을 출력해보자.
         for (int i = M; i <= N; i++){
-            if(!isPrime[i]){
+            if(!isNotPrime[i]){
                 System.out.println(i);
             }
         }
+
     }
 }
