fc63cd08e51d10e5fc64683a98917b65f42daaf2
YG <ce.walf@gmail.com>
2024-08-28 18:11:49 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.04 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -4,33 +4,33 @@ class Solution {
         int[] answer = {0, 0};
         
         // board의 크기를 벗어난 방향키 입력은 무시된다.
-        int boardWidth = board[0] / 2;
-        int boardHeight = board[1] / 2;
+        // 최대로 움직일 수 있는 x값, y값을 구한다.
+        int maxX = board[0] / 2;
+        int maxY = board[1] / 2;
         
-        // 키 입력을 순회한다.
-        int x = 0;
-        int y = 0;
+        // 키 입력을 순회한다. (up, down, left, right)
         for (String input : keyinput){
-            x = answer[0];
-            y = answer[1];
             switch(input) {
                 case "up":
-                    y++;
+                    if (answer[1] < maxY){
+                        answer[1]++;   
+                    }
                     break;
                 case "down":
-                    y--;
+                    if (answer[1] > -maxY){
+                        answer[1]--;   
+                    }
                     break;
                 case "left":
-                    x--;
+                    if (answer[0] > -maxX){
+                        answer[0]--;   
+                    }
                     break;
                 case "right":
-                    x++;
+                    if (answer[0] < maxX){
+                        answer[0]++;   
+                    }
                     break;
-            }
-            
-            if (x >= -boardWidth && x <= boardWidth && y <= boardHeight && y >= -boardHeight) {
-                answer[0] = x;
-                answer[1] = y;
             }
         }
         
