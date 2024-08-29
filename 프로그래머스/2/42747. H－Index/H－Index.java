7e1e98b7c18af3e5b2826dce1b097adc58068d97
YG <ce.walf@gmail.com>
2024-08-29 21:12:43 +0900

    [level 2] Title: H-Index, Time: 35.74 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -12,9 +12,9 @@ class Solution {
         
         // 가장 많이 인용된 횟수, 가장 적게 인용된 횟수
         int max = citations[n-1];
-        int min = citations[0];
 
         // 반복문으로 이를 찾아보자.
+        // [4,4,4] 같은 케이스도 찾아야 하므로, h >= 0을 조건식으로 설정한다.
         for (int h = max; h >= 0; h--){
             // h번 이상 인용된 논문, h번 이하 인용된 논문 찾기
             int countMore = 0;
@@ -29,7 +29,7 @@ class Solution {
             // 만약 h번 이상 인용된 논문이 h편 이상이고
             // 나머지 논문이 h번 이하 인용되었다면 
             // 답을 해당 시점의 h로 설정하고 반복문 탈출
-            if (countMore >= h && countBelow < h){
+            if (countMore >= h && countBelow <= h){
                 answer = h;
                 break;
             }
