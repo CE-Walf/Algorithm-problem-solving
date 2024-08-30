a6730376bffb933f80e0a054c3964ba2282a7716
YG <ce.walf@gmail.com>
2024-08-30 16:59:49 +0900

    [level 1] Title: 모의고사, Time: 1.00 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,50 +1,45 @@
+// 리팩토링 해보자.
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] answers) {
+        // 수포자 1 -> [1, 2, 3, 4, 5] 반복
+        int[] supoPattern1 = {1, 2, 3, 4, 5};
+        // 수포자 2 ->  [2, 1, 2, 3, 2, 4, 2, 5] 반복
+        int[] supoPattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        // 수포자 3 -> [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] 반복
+        int[] supoPattern3=  {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        // 1번 수포자가 찍는 방식 (1,2,3,4,5 반복)
-        int[] arr1 = {1,2,3,4,5};
-        // 2번 수포자가 찍는 방식 (2, 1, 2, 3, 2, 4, 2, 5 반복)
-        int[] arr2 = {2,1,2,3,2,4,2,5};
-        // 3번 수포자가 찍는 방식 (3, 3, 1, 1, 2, 2, 4, 4, 5, 5 반복)
-        int[] arr3 = {3,3,1,1,2,2,4,4,5,5};
-            
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
+        // 각 수포자들의 점수를 계산하는 배열
+        int[] scores = new int[3];
         
+        // 점수를 계산한다.
         for (int i = 0; i < answers.length; i++){
-            if(answers[i] == arr1[i % arr1.length]){
-                count1++;
-            }
-            if (answers[i] == arr2[i % arr2.length]){
-                count2++;
-            }
-            if (answers[i] == arr3[i % arr3.length]){
-                count3++;
-            }
+            if (answers[i] == supoPattern1[i % supoPattern1.length]) scores[0]++;
+            if (answers[i] == supoPattern2[i % supoPattern2.length]) scores[1]++;
+            if (answers[i] == supoPattern3[i % supoPattern3.length]) scores[2]++;
         }
         
-        int[] answer;
-        if (count1 == count2 && count2 == count3){
-            answer = new int[]{1, 2, 3};
-        } else if ((count1 == count2 && count1 > count3) || (count1 == count3 && count1 > count2) || (count2 == count3 && count2 > count1)){
-            if (count1 == count2 && count1 > count3){
-                answer = new int[]{1, 2};
-            } else if (count1 == count3 && count1 > count2){
-                answer = new int[]{1, 3};
-            } else {
-                answer = new int[]{2, 3};
-            }
-        } else {
-            if (count1 > count2 && count1 > count3){
-                answer = new int[]{1};
-            } else if (count2 > count3) {
-                answer = new int[]{2};
-            } else {
-                answer = new int[]{3};
+        // 최고 점수를 받은 수포자를 찾는다.
+        int maxScore = Math.max(scores[0], Math.max(scores[1], scores[2]));
+        
+        // 최고 점수를 받은 수포자를 찾아 List에 넣는다.
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < 3; i++){
+            if (scores[i] == maxScore){
+                result.add(i + 1);
             }
         }
-
-        return answer;
+        // 크기를 받는다.
+        int answerSize = result.size();
+        
+        // List를 배열로 변환한다.
+        int[] answer = new int[answerSize];
+        for (int i = 0; i < answerSize; i++){
+            answer[i] = result.get(i);
+        }
+        
+        // 결과값 반환
+        return answer; 
     }
 }
