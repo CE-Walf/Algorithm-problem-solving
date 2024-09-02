b02cd6ca2310803328c700fa2684c8ce13388947
YG <ce.walf@gmail.com>
2024-09-02 21:51:12 +0900

    [level 1] Title: 최소직사각형, Time: 2.01 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,35 +2,21 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[][] sizes) {
-        // 명함의 갯수
-        int n = sizes.length;
+        // 두 변 중에서 큰 변의 최댓값과 작은 변의 최댓값을 저장할 변수
+        int maxWidth = 0;
+        int maxHeight = 0;
         
-         // 명함의 가로 및 세로 길이 중 큰 값과 작은 값을 각각 저장할 배열을 선언한다.
-        int[] bigLength = new int[n]; // 큰 값을 저장할 배열
-        int[] smallLength = new int[n]; // 작은 값을 저장할 배열
-        
-        // 각 명함의 가로와 세로 길이 중 큰 값은 bigLength에,
-        // 작은 값은 smallLength에 저장한다.
-        for (int i = 0; i < n; i++){
-            int width = sizes[i][0];
-            int height = sizes[i][1];
+        for (int[] size : sizes) {
+            // 큰 값은 width로, 작은 값은 height로 설정
+            int width = size[0] > size[1] ? size[0] : size[1];
+            int height = size[0] > size[1] ? size[1] : size[0];
             
-            // 큰 값은 bigLength
-            // 작은 값은 smallLength에 저장
-            if (width > height){
-                bigLength[i] = width;
-                smallLength[i] = height;
-            } else {
-                bigLength[i] = height;
-                smallLength[i] = width;
-            }
+            // 최댓값 갱신
+            maxWidth = Math.max(maxWidth, width);
+            maxHeight = Math.max(maxHeight, height);
         }
         
-        // 두 배열을 정렬시켜 준다.
-        Arrays.sort(bigLength);
-        Arrays.sort(smallLength);
-        
-        // 큰 값과 작은 값 배열의 최대 값을 곱하여 준게 답이다.
-        return bigLength[n-1] * smallLength[n-1];
+        // 둘을 곱한 것이 답
+        return maxWidth * maxHeight;
     }
 }
