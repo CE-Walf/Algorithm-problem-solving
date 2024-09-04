6e9907121b7cd65e69581727b34231ec7b3b0234
YG <ce.walf@gmail.com>
2024-09-04 18:03:29 +0900

    [level 1] Title: 폰켓몬, Time: 4.59 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,29 +1,21 @@
-import java.util.Arrays;
+// HashSet을 사용하여 풀어보자.
+import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        // 배열을 정렬해준다. (같은 종류의 폰켓몬끼리 인접하게)
-        Arrays.sort(nums);
+        // HashSet 선언
+        Set<Integer> typeOfPonkemon = new HashSet<>();
         
-        // 가져갈 수 있는 최대 폰캣몬의 수는 전체 폰켓몬 수의 절반
-        int n = nums.length / 2;
-        
-        // 첫 번째 폰캣몬 종류는 무조건 선택되므로 n--
-        n--;
-        
-        // 나머지 배열 요소를 순회하며, 다른 종류의 폰켓몬이 선택될 때마다 n을 줄인다.
-        for (int i = 1; i < nums.length; i++){
-            // 가져갈 수 있는 폰켓몬의 수가 다 차면 반복문 탈출
-            if (n == 0) break;
-            
-            // 현재 폰켓몬이 이전 폰켓몬과 다른 종류면 n--
-            if (nums[i] != nums[i-1]){
-                n--;
-            }
+        // 배열의 내용을 Set에 넣어 중복을 제거시켜준다.
+        for (int num : nums){
+            typeOfPonkemon.add(num);
         }
         
-        // 가져갈 수 있는 최대 폰켓몬의 종류는
-        // 가져갈 수 있는 최대 폰켓몬의 수에서 남은 n을 뺀 값
-        return nums.length / 2 - n; 
+        // 가져갈 수 있는 최대 폰켓몬의 수
+        int maxPonkemon = nums.length / 2;
+        
+        // 가져갈 수 있는 최대 폰켓몬의 수와,
+        // 주어진 폰켓몬 종류의 수 중 작은 값이 답이다.
+        return Math.min(maxPonkemon, typeOfPonkemon.size());
     }
 }
