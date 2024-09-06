0dd25f5fd105bbd7a3aa0e9fbeb40e7dbc720246
YG <ce.walf@gmail.com>
2024-09-06 22:00:05 +0900

    [level 2] Title: 기능개발, Time: 3.45 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,39 +1,45 @@
+// 큐를 사용하여 풀어보자.
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        // 전체 기능의 갯수
         int n = progresses.length;
         
-        int[] completeDays = new int[n];
+        // 기능 완성까지 남은 일을 담을 Queue 생성
+        Queue<Integer> completionDates = new LinkedList<>();
         
+        // 완성일을 반복문을 통해 담아준다.
         for (int i = 0; i < n; i++){
-            int remainProgress = 100 - progresses[i];
-            int speed = speeds[i];
-            int completeDay = remainProgress / speed;
-            completeDays[i] = remainProgress % speed == 0 ? completeDay : completeDay + 1;
+            int remainingProgress = 100 - progresses[i]; // 남은 진행도
+            int daysToComplete = (int)Math.ceil((double)remainingProgress / speeds[i]); // 기능 완성까지 남은 일
+            completionDates.add(daysToComplete);
         }
+        // 배열의 크기를 알지 못하니, ArrayList 선언
+        List<Integer> result = new ArrayList<>();
         
-        List<Integer> deployFunction = new ArrayList<>();
-        
-        int count = 1;
-        int max = completeDays[0];
-        for (int i = 1; i < n; i++){
-            if (max < completeDays[i]){
-                max = completeDays[i];
-                deployFunction.add(count);
-                count = 1;
-                continue;
+        // 각 배포날에 배포될 작업의 수 계산
+        while (!completionDates.isEmpty()) {
+            // 첫 번째 작업의 완료 날짜를 가져온다.
+            int currentDate = completionDates.poll();
+            // 현재 날짜에 배포될 작업의 수
+            int count = 1;
+            
+            // 큐에서 완료 날짜가 currentDate보다 작거나 같은 작업의 수를 계산
+            // 작거나 같다면, 큐에서 꺼내고, 배포될 작업의 수는 1 추가
+            while (!completionDates.isEmpty() && completionDates.peek() <= currentDate) {
+                completionDates.poll(); 
+                count++; 
             }
-            count++;
-        }
-        deployFunction.add(count);
-        
-        int deploySize = deployFunction.size();
-        int[] answer = new int[deploySize];
-        for (int i = 0; i < deploySize; i++){
-            answer[i] = deployFunction.get(i);
+            
+            // 현재 날짜에 재포될 작업 수를 결과 리스트에 추가
+            result.add(count);
         }
         
-        return answer;
+        // Stream을 통해 List를 배열로 바꾸어 반환한다.
+        // result.stream() : List<Integer>를 스트림으로 변환
+        // mapToInt(i -> i)를 사용하여 각 Integer 객체를 기본형 int로 변환
+        // toArray() : IntStream을 int 배열로 변환 
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
