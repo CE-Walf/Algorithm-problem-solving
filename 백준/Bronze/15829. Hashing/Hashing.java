00f8a36e5943a1bad87ed42ba00545d55d93f5fe
WALF <ce.walf@gmail.com>
2024-10-18 20:30:00 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14200 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -2,23 +2,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 모듈러 연산의 성질 참고
+// https://zzinise.tistory.com/73
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int L = Integer.parseInt(br.readLine());
-
         String word = br.readLine();
 
         long answer = 0;
+        long pow = 1; // 31^0
 
-        for (int i = 0; i < word.length(); i++){
-            int intVal = word.charAt(i) - 'a' + 1;
-            answer += (long) (intVal * Math.pow(31, i));
+        for (int i = 0; i < L; i++){
+            answer += (word.charAt(i) - 'a' + 1) * pow;
+            pow = (pow * 31) % 1234567891;
         }
 
-        System.out.println(answer);
+        System.out.println(answer % 1234567891);
 
     }
 }
