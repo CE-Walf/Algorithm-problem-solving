a6eac4ed01f8356ec9231c85e948971d6a74f55e
WALF <ce.walf@gmail.com>
2024-10-31 20:23:22 +0900

    [level 2] Title: 타겟 넘버, Time: 5.05 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 1.40 ms
+메모리: 74.2 MB, 시간: 5.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 16:52:29
+2024년 10월 31일 20:23:19
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,21 @@
+class Solution {
+    static int answer = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        
+        return answer;
+    }
+    
+    static void dfs(int numbers[], int target, int count, int sum) {
+        if (count == numbers.length) {
+            if(sum == target){
+                answer++;
+                return;
+            } 
+        } else {
+            dfs(numbers, target, count + 1, sum + numbers[count]);
+            dfs(numbers, target, count + 1, sum - numbers[count]);
+        }
+    }
+}
