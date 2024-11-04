5d6d00f4860134bd5fc1de24169b13050d26ce3b
WALF <ce.walf@gmail.com>
2024-11-04 21:30:25 +0900

    [level 2] Title: 가장 큰 수, Time: 2898.15 ms, Memory: 407 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 132 MB, 시간: 284.79 ms
+메모리: 407 MB, 시간: 2898.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 29일 17:33:49
+2024년 11월 04일 21:30:23
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,30 +1,30 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        // 정답을 담을 변수
         String answer = "";
         
-        // 정수 배열을 문자열 배열로 바꾸어준다.
+        // 우선 정수 배열을 문자배열로 바꾸자.
         String[] strNumbers = new String[numbers.length];
-        for (int i = 0; i < strNumbers.length; i++) {
-            strNumbers[i] = String.valueOf(numbers[i]);
+        
+        int index = 0;
+        for (int number : numbers) {
+            strNumbers[index++] = String.valueOf(number);
         }
         
-        // 정렬 순서를 정하여 정렬해준다.
-        // (o1 + o2)가 (o2 + o1)보다 크면 o1이 o2보다 앞에 오게 된다.
-        // 반대로 (o1 + o2)가 (o2 + o1)보다 작으면 o2가 o1보다 앞에 오게 된다.
-        Arrays.sort(strNumbers, (o1, o2) -> (o2 + o1).compareTo(o1+o2));
-        
-        // 만약 맨 앞이 0이라면, 0을 반환해준다.
-        if (strNumbers[0].equals("0")){
-            answer = "0";
-        } else { // 아니라면, 해당 문자열 배열을 이어 붙어 문자열로 만들어준다.
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < strNumbers.length; i++){
-                sb.append(strNumbers[i]);
+        Arrays.sort(strNumbers, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
             }
-            answer = sb.toString();   
+        });
+        
+        if (strNumbers[0].equals("0")) {
+            return "0";
+        } 
+        
+        for (String strNumber : strNumbers) {
+            answer += strNumber;
         }
         
         return answer;
