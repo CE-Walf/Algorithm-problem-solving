49de9ee25a4929e08a4ee7c2b778114ff1efd856
WALF <ce.walf@gmail.com>
2024-11-04 21:03:07 +0900

    [level 1] Title: K번째수, Time: 0.51 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,35 +1,27 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        // 명령의 갯수만큼의 answer 배열 생성
-        int[] answer = new int[commands.length];
+        int n = commands.length;
+        int[] answer = new int[n];
         int i, j, k;
         
-        // i 번째 숫자부터, j 번째 숫자까지 자르고 정렬했을때, k번째에 있는 수 대입
-        for (int a = 0; a < commands.length; a++){
+        for (int a = 0; a < n; a++) {
             i = commands[a][0];
             j = commands[a][1];
             k = commands[a][2];
             
-            // j-i+1 의 크기로 임시 배열을 생성한다.
-            int[] temp = new int[j-i+1];
-            
-            // array에서 i번째 숫자부터 j번째 숫자까지 대입한다.
+            int[] tempArr = new int[j-i+1];
             int index = 0;
-            for (int b = i-1; b < j; b++){
-                temp[index++] = array[b];
+            for (int b = i-1; b < j; b++) {
+                tempArr[index++] = array[b];
             }
             
-            // 해당 배열을 정렬한다.
-            Arrays.sort(temp);
-            
-            // k번째 수를 answer 배열에 넣는다.
-            answer[a] = temp[k-1];
-            
+            Arrays.sort(tempArr);
+
+            answer[a] = tempArr[k-1];
             
         }
-        
         return answer;
     }
 }
