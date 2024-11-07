a396956136767de67a66982a73b573260e22d0df
WALF <ce.walf@gmail.com>
2024-11-07 21:03:14 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18024 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -3,37 +3,44 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // N을 1로 만들기 위한 최소 연산 횟수를 구하는 것이 문제이다.
+        // 1 <= N <= 10^6
         int N = Integer.parseInt(br.readLine());
 
-        if (N == 1) {
-            System.out.println(0);
-        } else if (N == 2) {
-            System.out.println(1);
-        } else if (N == 3) {
-            System.out.println(1);
-        } else {
-            int[] X = new int[N + 1];
+        // 각 숫자까지 도달하는 최소 연산 횟수를 저장할 배열 X
+        int[] X = new int[N + 1];
 
+        // 기저 조건
+        if (N >= 2) {
             X[2] = 1;
-            X[3] = 1;
-            
-            for (int i = 4; i <= N; i++) {
-                X[i] = X[i - 1] + 1;
-                if (i % 2 == 0) {
-                    X[i] = Math.min(X[i], X[i / 2] + 1);
-                }
-
-                if (i % 3 == 0) {
-                    X[i] = Math.min(X[i], X[i / 3] + 1);
-                }
-            }
-
-            System.out.println(X[N]);
         }
 
+        if (N >= 3) {
+            X[3] = 1;
+        }
+
+        // 4부터 N까지 연산 횟수를 채운다.
+        for (int i = 4; i <= N; i++) {
+            // 3. 1을 빼는 연산으로 접근하는 경우
+            X[i] = X[i - 1] + 1;
+
+            // 1. i가 3으로 나누어 떨어지는 경우, 최소 연산 횟수 갱신
+            if (i % 3 == 0) {
+                X[i] = Math.min(X[i], X[i / 3] + 1);
+            }
+
+            // 2. i가 2으로 나누어 떨어지는 경우, 최소 연산 횟수 갱신
+            if (i % 2 == 0) {
+                X[i] = Math.min(X[i], X[i / 2] + 1);
+            }
+        }
+
+        // 답 출력
+        System.out.println(X[N]);
 
     }
 }
