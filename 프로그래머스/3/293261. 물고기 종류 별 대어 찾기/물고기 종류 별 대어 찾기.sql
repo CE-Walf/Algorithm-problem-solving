c5fb3de32be9739e08e07b0bb92954126c421a9b
WALF <ce.walf@gmail.com>
2024-11-27 21:35:16 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,21 @@
+SELECT 
+    FI.ID,
+    FNI.FISH_NAME,
+    FI.LENGTH
+FROM 
+    FISH_INFO AS FI
+INNER JOIN
+    FISH_NAME_INFO AS FNI
+ON 
+    FI.FISH_TYPE = FNI.FISH_TYPE
+WHERE 
+    FI.LENGTH = (
+        SELECT
+            MAX(LENGTH)
+        FROM
+            FISH_INFO
+        WHERE
+            FISH_TYPE = FI.FISH_TYPE
+    )
+ORDER BY
+    ID ASC;
