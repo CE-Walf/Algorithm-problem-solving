2e7564484df1e7303f6e2ade64d0845638f54d12
WALF <ce.walf@gmail.com>
2025-02-20 13:50:32 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,46 +1,27 @@
-// 가장 먼저 탈락하는 사람의 번호와 그 사람이 자신의 몇 번째 차례에 탈락하는지를 출력하는 것
-// 만약 주어진 단어들로 탈락자가 생기지 않는다면, [0, 0]을 return 해주세요
-// 정답은 [ 번호, 차례 ] 형태로 return 해주세요.
-// 값 존재 유무 : List contains
-
 import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer;
-        List<String> appeared = new ArrayList<>(); // 등장한 단어를 담을 배열
+        int[] answer = {0, 0};
         
-        // 번호와 차례 (기본값 : 0)
-        int number = 0;
-        int order = 0;
+        char endCh = words[0].charAt(words[0].length() - 1);
+        char startCh;
         
-        // 이전 단어의 마지막 문자
-        char lastAlphabet = ' ';
+        Set<String> wordSet = new HashSet<>();
+        wordSet.add(words[0]);
         
-        for (int i = 1; i <= words.length; i++) {
-            // n명의 사람 (틀린 사람을 n으로 나눈 몫 = 차례, 나머지 = 번호)
-            String word = words[i-1]; 
-            if (appeared.contains(word)) {
-                number = (i - 1) % n + 1;
-                order = (i - 1) / n + 1;
+        for (int i = 1; i < words.length; i++) {
+            startCh = words[i].charAt(0);
+            wordSet.add(words[i]);
+            
+            if (endCh != startCh || wordSet.size() != i + 1) {
+                answer[0] = i % n + 1;
+                answer[1] = i / n + 1;
                 break;
             }
             
-            appeared.add(word);
-            
-            if (i > 1) {
-                if (lastAlphabet != word.charAt(0)){
-                    number = (i - 1) % n + 1;
-                    order = (i - 1) / n + 1;
-                    break;
-                }
-            }
-
-            lastAlphabet = word.charAt(word.length() - 1);
-            
+            endCh = words[i].charAt(words[i].length() - 1);
         }
-        
-        answer = new int[]{number, order};
         
         return answer;
     }
