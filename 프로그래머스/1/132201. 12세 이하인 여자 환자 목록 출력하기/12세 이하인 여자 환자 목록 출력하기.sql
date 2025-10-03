7d07ccc5daf7984caa723eab964ca7b1c55db243
WALF <ce.walf@gmail.com>
2025-10-03 17:04:24 +0900

    [level 1] Title: 12세 이하인 여자 환자 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
+++ 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
@@ -1,14 +1,13 @@
-SELECT
-    PT_NAME,
-    PT_NO,
-    GEND_CD,
-    AGE,
-    IFNULL(TLNO, 'NONE') 
-FROM 
+-- https://velog.io/@vgo_dongv/Oracle-SQL-NULL-%EC%B2%98%EB%A6%AC-%ED%95%A8%EC%88%98
+-- NVL(col, val) = col이 NULL이면 val을 반환하고, 아니면 col를 반환한다.
+
+SELECT 
+    PT_NAME, PT_NO, GEND_CD, AGE, NVL(TLNO, 'NONE') TLNO
+FROM
     PATIENT
 WHERE 
-    AGE <= 12 AND 
+    AGE <= 12
+AND
     GEND_CD = 'W'
-ORDER BY 
-    AGE DESC,
-    PT_NAME ASC;
+ORDER BY
+    AGE DESC, PT_NAME;
--- 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/README.md
+++ 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 1] 12세 이하인 여자 환자 목록 출력하기 - 132201 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132201) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132201?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 11월 27일 17:04:04
+2025년 10월 03일 17:04:23
 
 ### 문제 설명
 
